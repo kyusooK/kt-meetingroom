3793c4cde80b4c97a6f2345c82812d50806510b6
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2025-02-25 13:49:32 +0900

    commit

--- reservationmanagement/src/main/java/meetingroom/domain/Reservation.java
+++ reservationmanagement/src/main/java/meetingroom/domain/Reservation.java
@@ -43,6 +43,15 @@ public class Reservation {
 
     @PostPersist
     public void onPostPersist() {
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        meetingroom.external.Reservation reservation = new meetingroom.external.Reservation();
+        // mappings goes here
+        ReservationmanagementApplication.applicationContext
+            .getBean(meetingroom.external.ReservationService.class)
+            .createReservation(reservation);
+
         ReservationCreated reservationCreated = new ReservationCreated(this);
         reservationCreated.publishAfterCommit();
 
